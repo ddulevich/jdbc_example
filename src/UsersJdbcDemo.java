8a08049f9b2630bdeebb8b984da6729ccf2d78c3
ddulevich <dima27dulevich@gmail.com>
2020-01-20 17:35:36 +0300

    some refactoring

--- src/UsersJdbcDemo.java
+++ src/UsersJdbcDemo.java
@@ -9,26 +9,18 @@ public class UsersJdbcDemo {
     static final String PASSWORD = "root";
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Connection connection = null;
-        Statement statement = null;
-
-        System.out.println("Registering JDBC driver...");
-
-        Class.forName("com.mysql.jdbc.Driver");
-
-        System.out.println("Creating database connection...");
-        connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+        Class.forName(JDBC_DRIVER);
+        Connection connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
 
         System.out.println("Executing statement...");
-        statement = connection.createStatement();
+        Statement statement = connection.createStatement();
 
-        String sql;
-        sql = "SELECT * FROM user";
+        String sql = "SELECT * FROM user";
 
         ResultSet resultSet = statement.executeQuery(sql);
 
-        System.out.println("Retrieving data from database...");
         System.out.println("\nUsers:");
+
         while (resultSet.next()) {
             int id = resultSet.getInt("id");
             String name = resultSet.getString("login");
@@ -40,7 +32,6 @@ public class UsersJdbcDemo {
             System.out.println("email: " + specialty);
         }
 
-        System.out.println("Closing connection and releasing resources...");
         resultSet.close();
         statement.close();
         connection.close();
