6e75449bd88d70b392e46194071bfd5162832c90
ddulevich <DDS78965Winnert>
2020-01-20 21:51:01 +0300

    sec

--- src/UsersJdbcDemo.java
+++ src/UsersJdbcDemo.java
@@ -2,39 +2,88 @@ import java.sql.*;
 
 public class UsersJdbcDemo {
 
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DATABASE_URL = "jdbc:mysql://localhost/production_eshop?serverTimezone=UTC";
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost/eshop?serverTimezone=UTC";
 
-    static final String USER = "root";
-    static final String PASSWORD = "root";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName(JDBC_DRIVER);
-        Connection connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+    public static void main(String[] args) {
 
-        System.out.println("Executing statement...");
-        Statement statement = connection.createStatement();
+        registerDriver();
 
-        String sql = "SELECT * FROM user";
+        executeQuery();
 
-        ResultSet resultSet = statement.executeQuery(sql);
+        executeUpdate();
 
-        System.out.println("\nUsers:");
-
-        while (resultSet.next()) {
-            int id = resultSet.getInt("id");
-            String name = resultSet.getString("login");
-            String specialty = resultSet.getString("email");
-
-            System.out.println("\n================\n");
-            System.out.println("id: " + id);
-            System.out.println("login: " + name);
-            System.out.println("email: " + specialty);
-        }
-
-        resultSet.close();
-        statement.close();
-        connection.close();
+//        preparedStatement();
     }
 
+    private static void preparedStatement() {
+        try(Connection connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD)){
+
+            String sql = "Select * from user where id = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, 1);
+
+            preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.getResultSet();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String email = resultSet.getString("email");
+
+                System.out.println("\n================\n");
+                System.out.println("id: " + id);
+                System.out.println("login: " + name);
+                System.out.println("email: " + email);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void executeQuery() {
+        try(Connection connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+                Statement statement = connection.createStatement()) {
+
+                System.out.println("Executing statement...");
+
+                try(ResultSet resultSet = statement.executeQuery("SELECT * FROM user")) {
+
+                    System.out.println("\nUsers:");
+
+
+                }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void executeUpdate() {
+        try(Connection connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+            Statement statement = connection.createStatement()) {
+
+            System.out.println("Executing statement...");
+
+            int i = statement.executeUpdate("update user set email = 'a1@gmail.com' where id = 1");
+
+            System.out.println("Количсество измененных записей: " + i);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void registerDriver() {
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
